378d8743035a55afbe65ecb6c77d07aa4c03a45c
Anass Nammal <anammal@e1r4p7.1337.ma>
2023-06-09 19:46:40 +0100

    done

--- ps_tester.sh
+++ ps_tester.sh
@@ -6,23 +6,17 @@ if [ "$#" -eq 0 ] || ! [[ $1 =~ ^[0-9]+$ ]] || ! [[ ${2:-1} =~ ^[0-9]+$ ]] || [ 
     echo -e "\t[number of tests] to run N tests; 1 if not specified"
     exit 1
 fi
-
 input_size=$1
 num_tests=${2:-1}
-
-
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 if [ "$(uname)" == "Linux" ]; then
     CHECKER="./tool/checker_linux"
 else
     CHECKER="./tool/checker_Mac"
 fi
-
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-
 echo -e "test\tInput\tmoves\tChecker\tExec time\tMem leak"
-
 for ((i = 1; i <= num_tests; i++)); do
     ARG=$(ruby -e "puts (((-($input_size / 2)))..(($input_size / 2 - 1))).to_a.shuffle.join(' ')")
     moves=$(./push_swap $ARG | wc -l)
@@ -34,9 +28,8 @@ for ((i = 1; i <= num_tests; i++)); do
         leak_bytes=0
     fi
     color_code=$RED
-
     if [[ "$checker_output" == "OK" ]] &&
-        (( $(bc <<< "${exec_time%*s} < 0.030") )) &&
+        (( $(bc <<< "${exec_time%*s} < 0.050") )) &&
         [[ "$leak_bytes" == "0" ]] &&
         { [[ $input_size -eq 3 && $moves -lt 3 ]] ||
         [[ $input_size -eq 5 && $moves -lt 13 ]] ||
@@ -44,11 +37,6 @@ for ((i = 1; i <= num_tests; i++)); do
         [[ $input_size -le 500 && $moves -lt 5500 ]]; }; then
         color_code=$GREEN
     fi
-
-
-
-
-
     echo -e "${i}>\t${color_code}$input_size\t$moves\t$checker_output\t$exec_time\t\t$leak_bytes${NC}"
 done
 
